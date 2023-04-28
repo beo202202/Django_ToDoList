7cafe1e11064a20f4dd0e52cb0a7a0ffff2bdb31
Lee Juneyoung <wnsdud318@gmail.com>
2023-04-28 23:12:30 +0900

    Added the ability to delete a todolist

--- todo_lists/views.py
+++ todo_lists/views.py
@@ -34,18 +34,28 @@ class TodoListDetailView(APIView):
         return get_object_or_404(TodoList, pk=pk)
 
     def get(self, request, todolist_id):
+        #
         todolist = self.get_object(todolist_id)
+        #
         serializer = TodoListSerializer(todolist)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, todolist_id):
-        # 이사람 꺼인지 확인하고 권한 확인해야함.
         todolist = self.get_object(todolist_id)
-        serializer = TodoListCreateSerializer(todolist, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message":"수정완료!"}, status=status.HTTP_201_CREATED)
+        if request.user == todolist.user:
+            serializer = TodoListCreateSerializer(todolist, data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message":"수정완료!"}, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"message":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
-        
-
+            return Response({"message":"권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+    
+    def delete(self, request, todolist_id):
+        todolist = self.get_object(todolist_id)
+        if request.user == todolist.user:
+            todolist.delete()
+            return Response({"message":"삭제완료!"}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"message":"권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
